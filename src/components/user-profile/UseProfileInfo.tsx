5f4798f03e3693457901d4da31189bdb116fe4b3
viachaslaumasilevich <mosilevich@gmail.com>
2026-01-21 10:42:45 -0800

    test

--- src/components/user-profile/UseProfileInfo.tsx
+++ src/components/user-profile/UseProfileInfo.tsx
@@ -1,12 +1,12 @@
 "use client"
 import Image from "next/image";
-// import { useGetProfileQuery } from "@/src/services/userApi";
+import { useGetProfileQuery } from "@/src/services/userApi";
 
 
 
 export const UseProfileInfo = () => {
-  // const {data} = useGetProfileQuery({})
-  // console.log("DATA:", data)
+  const {data} = useGetProfileQuery()
+  console.log("DATA:", data)
   return (
     <div className="flex flex-row items-center gap-x-2 p-3 w-full md:max-w-[360px]  bg-(--color-white) rounded-lg">
       <Image src="/avatar/avatar.png" width={82} height={82} alt="Фото" />
