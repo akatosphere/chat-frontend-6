8952541aa2712760df8207c245b15b01896afcb4
viachaslaumasilevich <mosilevich@gmail.com>
2026-01-21 10:04:11 -0800

    refactor: section SettingProfilePhoto added

--- src/components/user-profile/profile/SettingsProfilePhoto.tsx
+++ src/components/user-profile/profile/SettingsProfilePhoto.tsx
@@ -5,6 +5,9 @@ const SettingsProfilePhoto = () => {
   return (
     <div className="items-center flex flex-col w-full justify-center p-4 gap-2">
       <Image src={avatar} width={200} height={200} alt="Фото" />
+      <button className="text-[color:var(--color-violet)] text-[1.125rem] font-medium hover:opacity-60 transition-opacity">
+         Изменить фото
+      </button>
     </div>
   );
 };
