baea131f186cb547a5cf521f905911a4c8c5f8cf
viachaslaumasilevich <mosilevich@gmail.com>
2026-01-21 10:08:37 -0800

    fix: arrow back changed

--- src/components/user-profile/profile/BackToSettings.tsx
+++ src/components/user-profile/profile/BackToSettings.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
-import backIcon from "@/assets/icons/back-icon.svg";
+import backIcon from "@/assets/icons/VectorBack.svg";
 
 const BackToSettings = () => {
   return (
     <div className="flex-row items-center gap-4 flex border-b border-[color:var(--color-gray-1)]">
       <Link href="/settings" className="flex p-4">
-        <Image src={backIcon} width={24} height={24} alt="" />
+        <Image src={backIcon} width={16} height={16} alt="" />
       </Link>
       <p className="flex justify-center text-(--color-black)  font-medium text-[1.125rem] ">
         Редактирование профиля
